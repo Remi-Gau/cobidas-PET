1f8defc6d97ddd59e16fa1fe1f9d958a3212ac93
Remi Gau <remi_gau@hotmail.com>
2020-08-08 07:00:22 +0200

    update README

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas-PET/master/protocols/PET/PET_schema',
-  contentSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas-PET/master/README.md',
   banner: 'This protocol is under construction.',
   startButton: 'Start',
   assetsPublicPath: '/cobidas-PET/',
