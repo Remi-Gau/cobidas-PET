f06f5a944098a8d99a47dd65f8dbf1a510ed3f28
Remi Gau <remi_gau@hotmail.com>
2021-06-26 16:38:15 +0200

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc:
-    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/schemas/pet/protocols/pet_schema.jsonld",
+    "https://raw.githubusercontent.com/ohbm/cobidas_schema/master/schemas/pet/protocols/pet_schema.jsonld",
   banner: "This protocol is under construction.",
   startButton: "Start",
   assetsPublicPath: "/cobidas-PET/",
